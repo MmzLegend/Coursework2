d542a0d580bbbe6d440465f3e9b120ac744cf067
Muhammad Mukhtar Zikirullahi <mukhtarzik@gmail.com>
2023-02-16 13:24:22 +0400

    first committ

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const fs = require("fs")
 const path = require("path")
 let dbConnection = require('./mongo.config')
 
+
+app.use(cors())
+
+//  logger middleware
+
+app.use((req,res, next)=>{
+    console.log("Request IP: "+req.url)
+    console.log("Request Date "+ new Date())
+    next();
+})
+
+//to avoid cors errror
+app.use ((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+})
+
 app.get('/lessons', async (req, res) => {
     console.log('getting lessons',req.query)
 
@@ -46,5 +68,29 @@ app.post('/increase-order', async (req, res) => {
         })
     }
 })
+
+app.put('/collection/:collectionName/:id', async function (req, res, next) {
+    await req.collection.updateOne(
+      { _id: new ObjectId(req.params.id) },
+      { $set: { space: req.body.space } },
+      (e, results) => {
+        res.json(results)
+        if (e) return next(e)
+    })
+  })
+  
+      // serving static files
+app.use((req,res,next)=>{
+    var filePath = path.join(__dirname, 'images',req.url)
+    fs.stat(filePath, function(err,fileInfo){
+        if(err){
+            next();
+            return ;
+        }
+        if(fileInfo.isFile) res.send(filePath)
+        else next()
+    })
+})
+
 
 app.listen(4400,() =>console.log)
