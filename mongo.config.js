bb7207a2bed722f83e315d1208d4e010b17f4267
MmzLegend <102227520+MmzLegend@users.noreply.github.com>
2023-02-16 13:57:20 +0400

    Delete mongo.config.js

--- mongo.config.js
+++
@@ -1,5 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://Mmzlegend23:Mxkht2721@cluster0.k3258d9.mongodb.net/test";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-module.exports = client;
